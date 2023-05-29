5f18c849998eb6975e1fa2a679eca54e66312c07
rittichok <rittichok.fa@gmail.com>
2023-05-29 23:36:22 +0700

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,15 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const data = [{
+      name: "aaa",
+      email: "aaa@mail",
+      tt: "test"
+    }];
+
+    // Convert the data array to a JSON string
+    const jsonString = JSON.stringify(data);
+
+    return jsonString;
   }
 }
