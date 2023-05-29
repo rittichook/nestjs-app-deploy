a9975598ab48f161b0dd602669abe07753a026af
rittichok <rittichok.fa@gmail.com>
2023-05-30 00:01:56 +0700

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,11 +3,48 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const data = [{
-      name: "aaa",
-      email: "aaa@mail",
-      tt: "test"
-    }];
+    const data = [
+      {
+      id: "1",
+      subject: "เทคโนโลยีปรับอากาศ",
+      dateStart: "08:00:00",
+      dateEnd: "12:00:00",
+      teacher: "A.Jarukit",
+      room: "ROOM A"
+    }
+    {
+      id: "2",
+      subject: "เทคโนโลยีปรับอากาศ",
+      dateStart: "08:00:00",
+      dateEnd: "12:00:00",
+      teacher: "A.Jarukit",
+      room: "ROOM A"
+    }
+    {
+      id: "3",
+      subject: "เทคโนโลยีปรับอากาศ",
+      dateStart: "08:00:00",
+      dateEnd: "12:00:00",
+      teacher: "A.Jarukit",
+      room: "ROOM A"
+    }
+    {
+      id: "4",
+      subject: "เทคโนโลยีปรับอากาศ",
+      dateStart: "08:00:00",
+      dateEnd: "12:00:00",
+      teacher: "A.Jarukit",
+      room: "ROOM A"
+    }
+    {
+      id: "5",
+      subject: "เทคโนโลยีปรับอากาศ",
+      dateStart: "08:00:00",
+      dateEnd: "12:00:00",
+      teacher: "A.Jarukit",
+      room: "ROOM A"
+    }
+  ];
 
     // Convert the data array to a JSON string
     const jsonString = JSON.stringify(data);
